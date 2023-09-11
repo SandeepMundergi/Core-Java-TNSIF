04a30ae363b95447f91eb4138b0d26f6145fcd44
sandeep <8453368444sandy@gmail.com>
2023-09-11 20:16:40 +0530

    Updated Polymorphism

---
+++ Polymorphism/src/com/tnsif/runtime/RateOfInterest.java
@@ -0,0 +1,24 @@
+package com.tnsif.runtime;
+
+public class RateOfInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RBI rate;
+
+		rate = new RBI();
+		System.out.println(rate.rateOfInterest());
+
+		rate = new Axis();
+		System.out.println(rate.rateOfInterest());
+
+		rate = new HDFC();
+		System.out.println(rate.rateOfInterest());
+
+		rate = new SBI();
+		System.out.println(rate.rateOfInterest());
+
+	}
+
+}
