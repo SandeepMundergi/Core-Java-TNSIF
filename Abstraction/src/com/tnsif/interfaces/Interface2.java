4fa012ec89441e10f7e7f597a5a2fa9dfaae36de
sandeep <8453368444sandy@gmail.com>
2023-09-11 20:08:43 +0530

    first commit

---
+++ Abstraction/src/com/tnsif/interfaces/Interface2.java
@@ -0,0 +1,6 @@
+package com.tnsif.interfaces;
+
+public interface Interface2 extends Interface1 {
+
+	public void print();
+}
---
+++ Abstraction/src/com/tnsif/markerinterface/StudentDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.markerinterface;
+
+public class StudentDemo {
+
+	public static void main(String args[]) {
+		// Student s1 = new Student();
+		Student.Registered();
+
+	}
+}
